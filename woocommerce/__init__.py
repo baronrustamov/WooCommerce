65e7d532be9b381c11fd3de120461dbf3273328d
Leon <ultrafunkamsterdam@users.noreply.github.com>
2019-10-08 12:53:25 +0200

    Update __init__.py

--- woocommerce/__init__.py
+++ woocommerce/__init__.py
@@ -16,6 +16,151 @@ __title__ = "woocommerce"
 __version__ = "1.0"
 __author__ = "UltrafunkAmsterdam"
 __license__ = "MIT"
-__all__ = ['Api']
+__all__ = ['Api', 'Object']
 
-from .api import Api
+import logging
+import requests
+from urllib.parse import urlparse
+import json
+
+
+class Api(object):
+
+    def __init__(self, site_url, consumer_key=None, consumer_secret=None, api='wp-json', version='wc/v3', debug=False):
+        """
+        Constructs an Api instance
+
+        Args:
+            site_url (str): the base site url (no paths!)
+            consumer_key (str): api consumer key
+            consumer_secret (str): api consumer secret
+            api (str): api name (defaults to wp-json)
+            version (str): version name (defaults to wc/v3)
+
+        Examples:
+            >>> api = Api('https://somesite.com', 'CK_string', 'CS_string')
+        """
+
+        self._log = logging.getLogger(self.__class__.__name__)
+        if debug:
+            self._log.setLevel(10)
+        p = urlparse(site_url)
+        if p.path:
+            site_url = site_url.split(p.path)[0]
+        self._consumer_key = consumer_key
+        self._consumer_secret = consumer_secret
+        self._site_url = site_url
+        self._version = version
+        self._api = api
+        self._api_url = f'{self._site_url}/{self._api}/{self._version}'
+        self._session = requests.session()
+        self._last_response = None
+        self._logged_in = False
+
+        self._params = {
+            "consumer_key": self._consumer_key,
+            "consumer_secret": self._consumer_secret
+        }
+
+        self._headers = {
+            "user-agent":
+                f"WooCommerce API Client-Python/{__version__}",
+            "accept":
+                "application/json",
+            "content-type":
+                "application/json;charset=utf-8"
+        }
+
+        self._session.headers.update(self._headers)
+        self._session.params.update(self._params)
+
+    def _set_endpoint(self, endpoint):
+        return endpoint[1:] if endpoint[0] == '/' else endpoint
+
+    def get(self, endpoint, limit=25):
+        params = f'?per_page={limit}'
+        r = self._last_response = self._session.get(self._api_url + f'/{self._set_endpoint(endpoint) + params}')
+        r = r.json()
+        if isinstance(r, list):
+            return [Object(x) for x in r]
+        return r
+
+    def post(self, endpoint, json_data):
+        r = self._last_response = self._session.post(self._api_url + f'/{self._set_endpoint(endpoint)}', json_data)
+        r = r.json()
+        if isinstance(r, list):
+            return [Object(x) for x in r]
+        return r
+
+
+    def put(self, endpoint, json_data):
+        r = self._last_response = self._session.put(self._api_url + f'/{self._set_endpoint(endpoint)}', json_data).json()
+        r = r.json()
+        if isinstance(r, list):
+            return [Object(x) for x in r]
+        return r
+
+    def delete(self, endpoint):
+        r = self._last_response = self._session.delete(self._api_url + f'/{self._set_endpoint(endpoint)}')
+        r = r.json()
+        if isinstance(r, list):
+            return [Object(x) for x in r]
+        return r
+
+    def generate(self, endpoint):
+        page = 1
+        while True:
+            params = f'?per_page=10&page={page}'
+            r = self._last_response = self._session.get(self._api_url + f'/{self._set_endpoint(endpoint) + params}')
+            products = r.json()
+            if not products:
+                break
+            for product in products:
+                yield Object(product)
+            page += 1
+
+
+class Object:
+
+    def __init__(self, data):
+        self._d = {}
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return self.__class__(value) if isinstance(value, dict) else value
+
+    def toDict(self):
+        return self._my_dict(self)
+
+    def toJson(self):
+        return json.dumps(self.toDict())
+
+    @classmethod
+    def fromDict(cls, d):
+        return cls(d)
+
+    @classmethod
+    def fromJson(cls, j):
+        import json
+        return cls(json.loads(j))
+
+    @staticmethod
+    def _my_dict(obj):
+        if not hasattr(obj, "__dict__"):
+            return obj
+        result = {}
+        for key, val in obj.__dict__.items():
+            if key.startswith("_"):
+                continue
+            element = []
+            if isinstance(val, list):
+                for item in val:
+                    element.append(Object._my_dict(item))
+            else:
+                element = Object._my_dict(val)
+            result[key] = element
+        return result
